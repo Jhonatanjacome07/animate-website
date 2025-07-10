086550101474ca0be07b07d30a86c152f51fb1ea
Jhonatan Jacome <jhonatanjacome07@gmail.com>
2025-07-10 16:52:11 -0500

    feat(hero): add scroll-triggered animation with GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
-import { TiLocationArrow } from "react-icons/ti";
-import { useEffect, useRef, useState } from "react";
+import { TiLocationArrow, TiEye } from "react-icons/ti";
 
 import Button from "./Button";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -60,9 +62,36 @@ const Hero = () => {
       revertOnUpdate: true,
     }
   );
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -122,8 +151,8 @@ const Hero = () => {
 
             <Button
               id="watch-trailer"
-              title="Watch trailer"
-              leftIcon={<TiLocationArrow />}
+              title="Ver Vehículos"
+              leftIcon={<TiEye />}
               containerClass="bg-yellow-300 flex-center gap-1"
             />
           </div>
@@ -131,7 +160,7 @@ const Hero = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>A</b>MING
+        <b>Mobility</b>
       </h1>
     </div>
   );
